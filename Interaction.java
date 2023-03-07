c6b3d28affc55dca26700006277d293a7a0dea8f
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-03-06 22:58:34 -0600

    Update Interaction.java

--- Interaction.java
+++ Interaction.java
@@ -10,11 +10,11 @@ public class Interaction {
 	}
 
     private int Operations() {
-		String[] options = {"Agregar un producto a la coleccion", "Mostrar la categoria del producto.", 
-				"Mostrar los datos del producto, categoria y la cantidad de cada articulo.", 
-				"Mostrar los datos del producto, categoria y la cantidad de cada articulo, ordenadas por tipo.",
-				"Mostrar el producto y la categoria de todo el inventario.",
-				"Mostrar el producto y la categoria existentes, ordenadas por tipo.","Salir."};
+		String[] options = {"Agregar un producto a la coleccion", "Mostrar la categoria del producto", 
+				"Mostrar los datos del producto, categoria y la cantidad de cada articulo", 
+				"Mostrar los datos del producto, categoria y la cantidad de cada articulo, ordenadas por tipo",
+				"Mostrar el producto y la categoria de todo el inventario",
+				"Mostrar el producto y la categoria existentes, ordenadas por tipo","Salir"};
 		for(int i = 0; i<options.length; i++) {
 			System.out.println((i+1 + ". " + options[i]));
 		}
